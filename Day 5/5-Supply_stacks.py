3195054ad3eb4e8a25f4c4f2bff58e3f970eb51b
rizu <trl.rizu2020@gmail.com>
2022-12-05 16:35:00 +0800

    new file:   5-Supply_stacks.py 	new file:   5-Supply_stacks.txt 	new file:   5-input.txt

---
+++ Day 5/5-Supply_stacks.py
@@ -0,0 +1,36 @@
+import sys
+
+with open('5-input.txt') as f:
+    data = f.read().strip()
+    lines = [x for x in data.split('\n')]
+
+S = []
+for line in lines:
+    if line == '':
+        break
+    sz = (len(line)+1)//4
+    while len(S) < sz:
+        S.append([])
+    for i in range(len(S)):
+        if line[1+4*i] != ' ' and 'A'<=line[1+4*i]<='Z':
+            S[i].append(line[1+4*i])
+# print(S)
+found = False
+for command in lines:
+    if command == '':
+        found = True
+        continue
+    if not found:
+        continue
+    words = command.split()
+    qty = int(words[1])
+    from_ = int(words[3])-1
+    to_ = int(words[5])-1
+    MOVE = S[from_][:qty]
+    S[from_] = S[from_][qty:]
+    #part one
+    S[to_] = list(reversed(MOVE)) + S[to_] 
+    #part two
+    # S[to_] = MOVE + S[to_]
+
+print(''.join([s[0] for s in S if len(s)>0]))
