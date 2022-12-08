8b031739604887651ae708a14eaecc057e5b5c51
rizu <trl.rizu2020@gmail.com>
2022-12-08 14:57:24 +0800

    modified:   8-treetop_treehouse.py

--- Day 8/8-treetop_treehouse.py
+++ Day 8/8-treetop_treehouse.py
@@ -11,6 +11,46 @@ DIR = [
         (1,0)      
     ]
 
+
+n = len(grid)
+m = len(grid[0])
+grid = np.array(grid)
+
+p1 = 0
+p2 = 0
+for i in range(n):
+    for j in range(m):
+        h = grid[i,j]
+        count = 1
+
+        if j == 0 or np.amax(grid[i, :j]) < h:
+            p1 += 1
+        elif j == m-1 or np.amax(grid[i, (j+1):]) < h:
+            p1 += 1
+        elif i == 0 or np.amax(grid[:i, j]) < h:
+            p1 += 1
+        elif i == n-1 or np.amax(grid[(i+1):, j]) < h:
+            p1 += 1
+
+        for di, dj in DIR:
+            ii, jj = i,j
+            dist = 0
+            ii += di
+            jj += dj
+            while (0 <= ii < n and 0 <= jj < m) and grid[ii, jj] < h:
+                dist += 1
+                ii += di
+                jj += dj
+                    
+                if (0 <= ii < n and 0 <= jj < m) and grid[ii][jj] >= h:
+                    dist += 1
+               
+            count *= dist
+        p2 = max(p2, count)
+
+print(p1, p2)
+
+#Original
 # height = len(lines)
 # width = len(lines[0])
 
@@ -55,45 +95,5 @@ DIR = [
 #             score *= dist
 #         p2 = max(p2, score)
 # print(p2)
-
-n = len(grid)
-m = len(grid[0])
-grid = np.array(grid)
-
-p1 = 0
-p2 = 0
-for i in range(n):
-    for j in range(m):
-        h = grid[i,j]
-        count = 1
-
-        if j == 0 or np.amax(grid[i, :j]) < h:
-            p1 += 1
-        elif j == m-1 or np.amax(grid[i, (j+1):]) < h:
-            p1 += 1
-        elif i == 0 or np.amax(grid[:i, j]) < h:
-            p1 += 1
-        elif i == n-1 or np.amax(grid[(i+1):, j]) < h:
-            p1 += 1
-
-        for di, dj in DIR:
-            ii, jj = i,j
-            dist = 0
-            ii += di
-            jj += dj
-            while (0 <= ii < n and 0 <= jj < m) and grid[ii, jj] < h:
-                dist += 1
-                ii += di
-                jj += dj
-                    
-                if (0 <= ii < n and 0 <= jj < m) and grid[ii][jj] >= h:
-                    dist += 1
-               
-            count *= dist
-        p2 = max(p2, count)
-
-print(p1, p2)
-
-
 
 
