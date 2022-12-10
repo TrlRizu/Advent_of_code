7eb21dd5427d60cb508795bd83d0e58202a9d1fb
rizu <trl.rizu2020@gmail.com>
2022-12-10 16:23:04 +0800

    new file:   Day 10/10-Cathode_ray_tube.py 	new file:   Day 10/10-Cathode_ray_tube.txt 	new file:   Day 10/10-input.txt 	new file:   Day 7/7-No_space_left_on_device.txt 	new file:   Day 8/8-treetop_treehouse.txt

---
+++ Day 10/10-Cathode_ray_tube.py
@@ -0,0 +1,54 @@
+with open('10-input.txt') as f:
+    lines = [x for x in f.read().strip().split('\n')]
+
+X,count,c = 1,0, [20, 60, 100, 140, 180, 220]
+p1 = 0
+p2 = [[None] * 40 for _ in range(6)]
+
+def draw(count, X):
+    global p2
+    _count = count-1
+    a,b = _count//40, _count%40
+    p2[a][b] = ('██' if abs(X-b) <= 1 else '  ')
+
+for line in lines:
+    cmd = line.strip().split()
+    if cmd[0] == 'addx':
+        count += 1
+        draw(count,X)
+
+        if count in c:
+            p1 += X * count
+        count += 1
+
+        if count in c:
+            p1 += X*count
+        draw(count,X)
+        X += int(cmd[1])    
+
+    elif cmd[0] == 'noop':
+        count += 1
+
+        if count in c:
+            p1 += X*count
+        draw(count,X)
+
+print(p1)
+
+for r in range(6):
+    print(''.join(p2[r]))
+
+# for c in range(40):
+#     for r in range(6):
+#         a,b = count//40, count%40
+#         counter = r * 40 + c + 1
+#         if abs(X-b) <= 1:
+#             p2[a][b] = '█' 
+#         else:
+#             p2[a][b] = ' ' 
+# print(p2)
+
+# if abs(X-b) <= 1:
+#             p2[a][b] = '█' 
+#         else:
+#             p2[a][b] = ' '
